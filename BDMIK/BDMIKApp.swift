98ee30ec4713cec182c148d5545f828a88c278f4
alanoodAlsh <94927860+alanoodAlsh@users.noreply.github.com>
2022-04-06 03:12:37 +0300

    Initial Commit

---
+++ BDMIK/BDMIKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDMIKApp.swift
+//  BDMIK
+//
+//  Created by Alanood Al Alsheikh on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BDMIKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BDMIKUITests/BDMIKUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BDMIKUITestsLaunchTests.swift
+//  BDMIKUITests
+//
+//  Created by Alanood Al Alsheikh on 06/04/2022.
+//
+
+import XCTest
+
+class BDMIKUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
